e8a9892fb9bc9a82f8c92c06394b2c18fcc546db
VanyaMate <buhu653@mail.ru>
2023-10-04 18:15:08 +0300

    almost create header with antd

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,57 @@
 import React from 'react';
-import { Category, ICategoriesService } from '@vanyamate/market-place-service';
+import {
+    Category,
+    ICategoriesService,
+    CategoriesService,
+    StorageService,
+    StorageName, IProductsService, Product,
+    ProductsLocalService,
+} from '@vanyamate/market-place-service';
+import categories
+    from '@vanyamate/market-place-service/data/categories/categories.json';
+import products_1
+    from '@vanyamate/market-place-service/data/products/products_1.json';
+import products_2
+    from '@vanyamate/market-place-service/data/products/products_2.json';
 import { useMemo } from 'react';
+import HeaderProductSearch
+    from '../HeaderProductSearch/HeaderProductSearch.tsx';
 import HeaderStoreName from '../HeaderStoreName/HeaderStoreName.tsx';
+import css from './Header.module.scss';
 
 
 const Header = () => {
     const categoriesService: ICategoriesService<Category> = useMemo(() => {
+        return new CategoriesService(
+            new StorageService(
+                localStorage,
+                StorageName.CATEGORIES,
+            ),
+            categories,
+        );
+    }, []);
 
+    const productsService: IProductsService<Product> = useMemo(() => {
+        return new ProductsLocalService(
+            new StorageService(
+                localStorage,
+                StorageName.PRODUCTS,
+            ),
+            products_1,
+            products_2,
+        );
     }, []);
 
     return (
-        <div>
-            <div>
+        <div className={ css.container }>
+            <div className={ css.left }>
                 <HeaderStoreName name={ 'MateStore' }/>
-                <div>
-                    search
-                    <input/>
-                </div>
+                <HeaderProductSearch
+                    categoriesService={ categoriesService }
+                    productsService={ productsService }
+                />
             </div>
-            <div>
+            <div className={ css.right }>
                 elements
             </div>
         </div>
--- src/components/HeaderProductSearch/HeaderProductSearch.tsx
+++ src/components/HeaderProductSearch/HeaderProductSearch.tsx
@@ -1,5 +1,10 @@
-import { Input, Select } from 'antd';
-import React from 'react';
+import { Divider, Dropdown, Input, Select } from 'antd';
+import React, {
+    useCallback,
+    useEffect,
+    useMemo,
+    useState,
+} from 'react';
 import {
     ICategoriesService,
     IProductsService,
@@ -7,6 +12,10 @@ import {
     Category,
 } from '@vanyamate/market-place-service';
 import css from './HeaderProductSearch.module.scss';
+import type { MenuProps } from 'antd';
+
+
+const { Search } = Input;
 
 
 export type HeaderProductSearchProps = {
@@ -14,14 +23,92 @@ export type HeaderProductSearchProps = {
     productsService: IProductsService<Product>
 }
 
-const HeaderProductSearch: React.FC<HeaderProductSearchProps> = () => {
-    return (
-        <div className={ css.container }>
-            <Select>
+const HeaderProductSearch: React.FC<HeaderProductSearchProps> = (props) => {
+    const { categoriesService, productsService } = props;
+    const [ categories, setCategories ]          = useState<Category[]>([]);
+    const [ products, setProducts ]              = useState<Product[]>([]);
+    const [ search, setSearch ]                  = useState<string>('');
+    const [ activeCategory, setActiveCategory ]  = useState<string | null>(null);
+    const categoryOptions                        = useMemo<{
+        value: string,
+        label: string
+    }[]>(() => {
+        return [
+            {
+                value: '',
+                label: 'All categories',
+            },
+            ...categories.map((category) => ({
+                value: category.title,
+                label: category.title,
+            })),
+        ];
+    }, [ categories ]);
 
-            </Select>
-            <Input/>
-        </div>
+    const productsOptions = useMemo<MenuProps['items']>(() => {
+        console.log('products options', products);
+        return products.map((product) => ({
+            key  : product.barcode.toString(),
+            label: (<div>{ product.product_name }</div>),
+        }));
+    }, [ products ]);
+
+    useEffect(() => {
+        if (search) {
+            const filter: Partial<Product> = {
+                product_name: search,
+            };
+            if (activeCategory) {
+                filter.category = activeCategory;
+            }
+            productsService
+                .findMany(filter, {
+                    limit: 10,
+                })
+                .then((response) => response.list)
+                .then((products) => setProducts(products));
+        }
+    }, [ search, activeCategory ]);
+
+    useEffect(() => {
+        categoriesService
+            .findMany({}, { limit: 30 })
+            .then((response) => response.list)
+            .then((categories) => setCategories(categories));
+    }, []);
+
+    const onCategoryChange = useCallback((category: string) => {
+        setActiveCategory(category ?? null);
+    }, [ activeCategory ]);
+
+    const onSearchChange = useCallback((search: string) => {
+        setSearch(search);
+    }, [ search ]);
+
+    return (
+        <Dropdown
+            menu={ { items: productsOptions } }
+            trigger={ [ 'hover' ] }
+        >
+            <div className={ css.container }>
+                <Select
+                    defaultValue={ '' }
+                    onChange={ onCategoryChange }
+                    options={ categoryOptions }
+                    bordered={ false }
+                    className={ css.select }
+                    size={ 'large' }
+                />
+                <Divider type={ 'vertical' } orientation={ 'center' }/>
+                <Search
+                    placeholder={ 'Search for your products' }
+                    bordered={ false }
+                    onSearch={ onSearchChange }
+                    size={ 'large' }
+                    className={ css.search }
+                />
+            </div>
+        </Dropdown>
     );
 };
 
